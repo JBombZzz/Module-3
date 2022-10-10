cc47f62c4ca32f738bc7c9f483d5b33409459444
JBombZzz <115471715+JBombZzz@users.noreply.github.com>
2022-10-10 16:00:56 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __James__
+
+# _I am interested in data science because I like data and I like science_
+
+# ### Cell 5 contains code
+
+# In[9]:
+
+
+1+1
+
+
+# <div class="alert alert-block alert-info">
+#     
+# > Indentation
+# - Bullet 1
+# - Bullet 2
+# 
+# 1. Numbered item
+# 2. Numbered item
